61c16fb8c17ec3cbf95c06b89e8dd437c97bc8c3
Daniele Perot <perotdaniele@gmail.com>
2025-04-26 18:15:55 +0300

    Remove code duplication

--- src/lexer.py
+++ src/lexer.py
@@ -25,53 +25,55 @@ class Lexer:
     _cursor: int = 0
     _tokens_iter: Generator[Optional[Token], None, None] = None
 
+    _token_value: str = ""
+    _token_type: TokenType = TokenType.ID
+
     def __init__(self, content: str):
         self._content = content
 
     def _tokens(self) -> Generator[Optional[Token], None, None]:
-        t_value = ""
-        t_type = TokenType.ID
+        self._token_value = ""
+        self._token_type = TokenType.ID
         i = 0
 
         while i < len(self._content):
             next_char = "" if i == len(self._content) else str(self._content[i])
 
             if next_char == '"':
-                if t_type != TokenType.STRING:
-                    t_type = TokenType.STRING
+                if self._token_type != TokenType.STRING:
+                    self._token_type = TokenType.STRING
                 else:
-                    yield Token(type=t_type, value=t_value)
-                    t_value = ""
-                    t_type = TokenType.ID
-            elif next_char.isspace() and t_type != TokenType.STRING:
-                if t_value:
-                    yield Token(type=t_type, value=t_value)
-                    t_value = ""
-                    t_type = TokenType.ID
-            elif next_char.isnumeric() and not t_value:
-                if t_type != TokenType.INTEGER:
-                    t_type = TokenType.INTEGER
-                t_value += next_char
-            elif next_char == "." and t_type == TokenType.INTEGER:
-                t_type = TokenType.FLOAT
-                t_value += next_char
-            elif next_char in KNOWN_SYMBOLS and t_type != TokenType.STRING:
+                    yield from self._yield_token_and_reset()
+            elif next_char.isspace() and self._token_type != TokenType.STRING:
+                if self._token_value:
+                    yield from self._yield_token_and_reset()
+            elif next_char.isnumeric() and not self._token_value:
+                if self._token_type != TokenType.INTEGER:
+                    self._token_type = TokenType.INTEGER
+                self._token_value += next_char
+            elif next_char == "." and self._token_type == TokenType.INTEGER:
+                self._token_type = TokenType.FLOAT
+                self._token_value += next_char
+            elif next_char in KNOWN_SYMBOLS and self._token_type != TokenType.STRING:
                 # first provide the current token, as it just terminated
-                if t_value:
-                    yield Token(type=t_type, value=t_value)
-                    t_value = ""
-                    t_type = TokenType.ID
+                if self._token_value:
+                    yield from self._yield_token_and_reset()
 
                 # next, provide the actual symbol
                 yield Token(type=TokenType.SYMBOL, value=next_char)
             else:
-                t_value += next_char
+                self._token_value += next_char
             i += 1
 
-        if t_value:
-            yield Token(type=t_type, value=t_value)
+        if self._token_value:
+            yield Token(type=self._token_type, value=self._token_value)
         else:
             yield None
+
+    def _yield_token_and_reset(self):
+        yield Token(type=self._token_type, value=self._token_value)
+        self._token_value = ""
+        self._token_type = TokenType.ID
 
     def next_token(self) -> Optional[Token]:
         if self._tokens_iter is None:
